3f89034216fb993c4929fb4b7c7822c3cd5bfd8b
Aravind I M <aravindim@yahoo.com>
2026-07-31 00:10:06 +0530

    feat(pong): added sdl3 window

---
+++ pong.cpp
@@ -0,0 +1,31 @@
+﻿// pong.cpp : Defines the entry point for the application.
+//
+
+#include "pong.h"
+
+int main(int argc, char* argv[])
+{
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", SDL_GetError(), nullptr);
+		return -1;
+	}
+	SDL_Window* window = SDL_CreateWindow("Pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+	if (!window)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", SDL_GetError(), nullptr);
+		cleanup(window);
+		return -1;
+	}
+	bool running = true;
+
+	while (running);
+
+	cleanup(window);
+	return 0;
+}
+
+void cleanup(SDL_Window* window) {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ pong.h
@@ -0,0 +1,13 @@
+﻿// pong.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <SDL3/SDL.h>
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+void cleanup(SDL_Window* window);
+
+// TODO: Reference additional headers your program requires here.
