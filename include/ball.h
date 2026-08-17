ceb820b7d6a10ec29c0e843320456fc0fad3e82f
Aravind I M <aravindim@yahoo.com>
2026-08-17 20:29:57 +0530

    feat(physics): added randomisation to initial movement of the ball and continue the same velocity on restart

--- include/ball.h
+++ include/ball.h
@@ -9,6 +9,9 @@
 // Coordinates
 #define BALL_START_X ((WINDOW_WIDTH - BALL_SIZE) / 2)
 #define BALL_START_Y ((WINDOW_HEIGHT - BALL_SIZE) / 2)
+#define BALL_START_Y_MIN_PERCENT 0.25
+#define BALL_START_Y_MAX_PERCENT 0.75
+
 
 // Bounds
 #define BALL_MIN_X 0
@@ -17,13 +20,14 @@
 #define BALL_MAX_Y (WINDOW_HEIGHT - BALL_SIZE)
 
 // Physics
-#define BALL_START_VX -400
-#define BALL_START_VY -200
+#define BALL_START_VX 250
+#define BALL_START_VY 250
 
 struct Ball {
 	SDL_FRect mRect;
 	float mVx;
 	float mVy;
+	bool mShow;
 	Ball();
 	void Reset();
 	void Move(double deltaTime);
--- src/ball.cpp
+++ src/ball.cpp
@@ -1,21 +1,36 @@
 #include "ball.h"
+#include <random>
 
-Ball::Ball() {
+Ball::Ball() :
+	mShow(false)
+{
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::bernoulli_distribution dist(0.5);
+	mVx = static_cast<float>(BALL_START_VX);
+	mVy = static_cast<float>(BALL_START_VY);
+	mVx = dist(rng) ? mVx : -mVx;
+	mVy = dist(rng) ? mVy : -mVy;
 	Reset();
 }
 
 void Ball::Reset() {
-	mRect = SDL_FRect(BALL_START_X, BALL_START_Y, BALL_SIZE, BALL_SIZE);
-	mVx = BALL_START_VX;
-	mVy = BALL_START_VY;
+	mShow = false;
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_real_distribution<float> dist(BALL_MIN_Y + (BALL_MAX_Y - BALL_MIN_Y) * BALL_START_Y_MIN_PERCENT, BALL_MIN_Y + (BALL_MAX_Y - BALL_MIN_Y) * BALL_START_Y_MAX_PERCENT);
+	mRect = SDL_FRect(BALL_START_X, dist(rng), BALL_SIZE, BALL_SIZE);
 }
 
 void Ball::Move(double deltaTime) {
+	mShow = true;
 	mRect.x = SDL_clamp(mRect.x + (float)(mVx * deltaTime), BALL_MIN_X, BALL_MAX_X);
 	mRect.y = SDL_clamp(mRect.y + (float)(mVy * deltaTime), BALL_MIN_Y, BALL_MAX_Y);
 }
 
 void Ball::Render(SDL_Renderer* renderer) {
-	SDL_SetRenderDrawColor(renderer, FG_COLOR, FG_COLOR, FG_COLOR, 0xFF);
-	SDL_RenderFillRect(renderer, &mRect);
+	if (mShow) {
+		SDL_SetRenderDrawColor(renderer, FG_COLOR, FG_COLOR, FG_COLOR, 0xFF);
+		SDL_RenderFillRect(renderer, &mRect);
+	}
 }
