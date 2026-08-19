456706f144ef0605051528316262adcbe24728e8
Aravind I M <aravindim@yahoo.com>
2026-08-19 20:11:52 +0530

    refactor(ball): extracted randomised Y into its own variable

--- src/ball.cpp
+++ src/ball.cpp
@@ -13,7 +13,8 @@ void Ball::Reset() {
 	mShow = false;
 	constexpr float minRandomY = BALL_MIN_Y + (BALL_MAX_Y - BALL_MIN_Y) * BALL_START_Y_MIN_PERCENT;
 	constexpr float maxRandomY = BALL_MIN_Y + (BALL_MAX_Y - BALL_MIN_Y) * BALL_START_Y_MAX_PERCENT;
-	mRect = SDL_FRect(BALL_START_X, random<std::uniform_real_distribution<float>>(minRandomY, maxRandomY), BALL_SIZE, BALL_SIZE);
+	float randomY = random<std::uniform_real_distribution<float>>(minRandomY, maxRandomY);
+	mRect = SDL_FRect(BALL_START_X, randomY, BALL_SIZE, BALL_SIZE);
 }
 
 void Ball::Move(float deltaTime) {
