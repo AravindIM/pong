94244a688d66ddccd4e9bdd9a9679d7cf8ab9b8e
Aravind I M <aravindim@yahoo.com>
2026-08-30 20:29:24 +0530

    fix(scoreboard): fixed the FRect for segments

--- include/scoreboard.h
+++ include/scoreboard.h
@@ -2,9 +2,11 @@
 #include "config.h"
 #include<SDL3/SDL.h>
 
+constexpr float SCORE_THICKNESS = 5.0;
+
 struct Scoreboard {
 	SDL_FRect mRect{};
-	float mThickness{};
+	float mThickness{ SCORE_THICKNESS };
 	Scoreboard() = default;
-	void RenderSegment(SDL_Renderer* renderer, Uint8 segment);
+	void RenderSegment(SDL_Renderer* renderer, Uint8 segment, SDL_FRect rect);
 };
--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -1,73 +1,41 @@
 #include "scoreboard.h"
 
-void Scoreboard::RenderSegment(SDL_Renderer* renderer, Uint8 segment) {
-	SDL_FRect sRect;
+void Scoreboard::RenderSegment(SDL_Renderer* renderer, Uint8 segment, SDL_FRect rect) {
 	switch (segment) {
 	case 0:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y,
-			.w = mRect.w,
-			.h = mThickness
-		};
+		rect.h = mThickness;
 		break;
 	case 1:
-		sRect = {
-			.x = mRect.x + (mRect.w - mThickness),
-			.y = mRect.y,
-			.w = mThickness,
-			.h = mRect.h / 2
-		};
+		rect.x += rect.w - mThickness;
+		rect.w = mThickness;
+		rect.h /= 2;
 		break;
 	case 2:
-		sRect = {
-			.x = mRect.x + (mRect.w - mThickness),
-			.y = mRect.y + mRect.h / 2,
-			.w = mThickness,
-			.h = mRect.h / 2
-		};
+		rect.x += rect.w - mThickness;
+		rect.y += rect.h / 2;
+		rect.w = mThickness;
+		rect.h /= 2;
 		break;
 	case 3:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y + (mRect.h - mThickness),
-			.w = mRect.w,
-			.h = mThickness
-		};
+		rect.y += rect.h - mThickness,
+		rect.h = mThickness;
 		break;
 	case 4:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y + (mRect.h - mThickness) / 2,
-			.w = mThickness,
-			.h = mRect.h / 2
-		};
+		rect.y += rect.h / 2;
+		rect.w = mThickness;
+		rect.h /=  2;
 		break;
 	case 5:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y,
-			.w = mThickness,
-			.h = mRect.h / 2
-		};
+		rect.w = mThickness;
+		rect.h /= 2;
 		break;
 	case 6:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y + mRect.h / 2,
-			.w = mRect.w,
-			.h = mThickness
-		};
+		rect.y += (rect.h - mThickness) / 2;
+		rect.h = mThickness;
 		break;
 	default:
-		sRect = {
-			.x = mRect.x,
-			.y = mRect.y,
-			.w = 0,
-			.h = 0
-		};
 		break;
 	}
 	SDL_SetRenderDrawColor(renderer, FG_COLOR, FG_COLOR, FG_COLOR, 0xFF);
-	SDL_RenderFillRect(renderer, &mRect);
+	SDL_RenderFillRect(renderer, &rect);
 }
